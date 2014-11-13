c9781de09ebc883fc9ca467293ef5cf7f77a7bd9
pollseed <cb.so.devope+0001@gmail.com>
2014-11-14 00:15:18 +0900

    stock data py modify

--- src/stock_data_mining.py
+++ src/stock_data_mining.py
@@ -1,10 +1,17 @@
 import matplotlib.pyplot as plt
 
-stock = sp.genfromtxt("stock_test.tsv", delimiter="\t")
+# 株情報8/10〜10/10までのやつを取得して分布させてみる
+stock = sp.genfromtxt("../data/stock_test.tsv", delimiter="\t")
 a = stock[:,0]
+
+# とりあえず終値だけ取得させる
 b = stock[:,2]
+
+# 壊れてる値弾く
 a = a[~sp.isnan(a)]
 b = b[~sp.isnan(b)]
+
+# 分布図で行こうと思う
 plt.scatter(a,b)
 plt.title("Stock data 8/10-10/10")
 plt.xlabel("prev_closing_price")
