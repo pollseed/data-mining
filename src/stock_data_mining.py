de2b99fadc3b53472ff947fe61459b0361246eb9
Seed <cb.so.devope+0001@gmail.com>
2014-11-14 16:46:58 +0900

    実行可能なクラスへ

--- src/stock_data_mining.py
+++ src/stock_data_mining.py
@@ -1,21 +1,28 @@
 import scipy as sp
 import matplotlib.pyplot as plt
 
-# 株情報8/10〜10/10までのやつを取得して分布させてみる
-stock = sp.genfromtxt("../data/stock_test.tsv", delimiter="\t")
-a = stock[:,0]
+def get_stock_data_csv():
+	# 株情報8/10〜10/10までのやつを取得して分布させてみる
+	return sp.genfromtxt("../data/stock_test.tsv", delimiter="\t")
 
-# とりあえず終値だけ取得させる
-b = stock[:,2]
+def conpare(x, y):
+	# 壊れてる値弾く
+	x = x[~sp.isnan(x)]
+	y = y[~sp.isnan(y)]
 
-# 壊れてる値弾く
-a = a[~sp.isnan(a)]
-b = b[~sp.isnan(b)]
+	# 分布図で行こうと思う
+	plt.scatter(x, y)
 
-# 分布図で行こうと思う
-plt.scatter(a,b)
-plt.title("Stock data 8/10-10/10")
-plt.xlabel("prev_closing_price")
-plt.ylabel("No.")
-plt.grid()
-plt.show()
+	# TODO 固定値にして呼ぶ
+	plt.title("Stock data 8/10-10/10")
+	plt.xlabel("prev_closing_price")
+	plt.ylabel("No.")
+	plt.grid()
+	plt.show()
+
+def main():
+	stock = get_stock_data_csv()
+	conpare(stock[:,0], stock[:,2])
+
+if __name__ == '__main__':
+	main()
