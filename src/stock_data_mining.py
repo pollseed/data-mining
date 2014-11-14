9b63b3bf17c3fc8391096dce5394a7aef65fbecc
Seed <cb.so.devope+0001@gmail.com>
2014-11-14 18:33:38 +0900

    Update stock_data_mining.py

--- src/stock_data_mining.py
+++ src/stock_data_mining.py
@@ -32,3 +32,73 @@ def main():
 if __name__ == '__main__':
 	main()
 	
+	
+#########################################################
+## 下テストコード用
+#########################################################
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import sys, csv, datetime
+
+# ストックデータをタブで区分けして返します.
+def get_stock_data_csv():
+	return sp.genfromtxt(
+		"../data/stock_test.tsv",
+		delimiter="\t",
+		skiprows=1,
+		dtype={
+		'names':(
+			'id',
+			'announce_date',
+			'prev_closing_price',
+			'opening_price',
+			'high_price',
+			'low_price',
+			'buy_flg',
+			'delete_flg',
+			'created',
+			'updated'),
+		'formats':(
+			'i4', 'S10',
+			'f8', 'f8', 'f8', 'f8', 'b', 'b',
+			'S21', 'S21')})
+
+# 指定したストックデータから列指向で指定引数を抜き取り返します.
+def get_stock_line(stock, x_number, y_number):
+	# for x in stock[:,x_number]:
+	# 	datetime.datetime(x)
+
+	# y = stock[:,y_number]
+	return (stock[:,x_number], stock[:,y_number])
+
+# 散布図を出力します.
+def scatter(x, y):
+	# 壊れてる値弾く
+	# x = x[~sp.isnan(x)]
+	print(x)
+	# y = y[~sp.isnan(y)]
+	print(y)
+
+	plt.scatter(x, y)
+
+	# TODO 固定値にして呼ぶ
+	plt.title("Stock data 8/10-10/10")
+	plt.xlabel("prev_closing_price")
+	plt.ylabel("No.")
+	plt.grid()
+	plt.show()
+
+def main():
+	# ストックデータを取得
+	stock = get_stock_data_csv()
+	# ストックデータから任意の列をもぐ
+	x, y = get_stock_line(stock, 1, 3)
+	# 散布図出力
+	scatter(x, y)
+
+if __name__ == '__main__':
+	main()
