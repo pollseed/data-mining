1b9ec39c9afaaa83ca6aae21069f863e628e2cdd
pollseed <cb.so.devope+0001@gmail.com>
2014-11-15 10:16:44 +0900

    一旦、tmpの方で。時間があれば要修正

--- src/stock_data_mining.py
+++ src/stock_data_mining.py
@@ -1,41 +1,3 @@
-import scipy as sp
-import matplotlib.pyplot as plt
-
-# ストックデータをタブで区分けして返します.
-def get_stock_data_csv():
-	return sp.genfromtxt("../data/stock_test.tsv", delimiter="\t")
-
-# 指定したストックデータから列指向で指定引数を抜き取り返します.
-def get_stock_line(stock, x_number, y_number):
-	return (stock[:,x_number], stock[:,y_number])
-
-# 散布図を出力します.
-def scatter(x, y):
-	# 壊れてる値弾く
-	x = x[~sp.isnan(x)]
-	y = y[~sp.isnan(y)]
-
-	plt.scatter(x, y)
-
-	# TODO 固定値にして呼ぶ
-	plt.title("Stock data 8/10-10/10")
-	plt.xlabel("prev_closing_price")
-	plt.ylabel("No.")
-	plt.grid()
-	plt.show()
-
-def main():
-	stock = get_stock_data_csv()
-	x, y = get_stock_line(stock, 0, 2)
-	scatter(x, y)
-
-if __name__ == '__main__':
-	main()
-	
-	
-#########################################################
-## 下テストコード用
-#########################################################
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -45,6 +7,7 @@ import matplotlib.pyplot as plt
 import sys, csv, datetime
 
 # ストックデータをタブで区分けして返します.
+# TODO うまく動作しない.原因は１次元配列になってしまっているから
 def get_stock_data_csv():
 	return sp.genfromtxt(
 		"../data/stock_test.tsv",
@@ -67,6 +30,9 @@ def get_stock_data_csv():
 			'f8', 'f8', 'f8', 'f8', 'b', 'b',
 			'S21', 'S21')})
 
+def get_stock_data_csv_tmp():
+	return sp.genfromtxt("../data/stock_test.tsv", delimiter="\t")
+
 # 指定したストックデータから列指向で指定引数を抜き取り返します.
 def get_stock_line(stock, x_number, y_number):
 	# for x in stock[:,x_number]:
@@ -79,8 +45,10 @@ def get_stock_line(stock, x_number, y_number):
 def scatter(x, y):
 	# 壊れてる値弾く
 	# x = x[~sp.isnan(x)]
+	print("x軸のデータを出力します")
 	print(x)
 	# y = y[~sp.isnan(y)]
+	print("y軸のデータを出力します")
 	print(y)
 
 	plt.scatter(x, y)
@@ -94,9 +62,9 @@ def scatter(x, y):
 
 def main():
 	# ストックデータを取得
-	stock = get_stock_data_csv()
+	stock = get_stock_data_csv_tmp()
 	# ストックデータから任意の列をもぐ
-	x, y = get_stock_line(stock, 1, 3)
+	x, y = get_stock_line(stock, 0, 3)
 	# 散布図出力
 	scatter(x, y)
 
